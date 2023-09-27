ab4ad046378f82c7c8890e38bec82cb4412f7db7
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-09-27 17:04:19 -0400

    changes to service controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const { route } = require("./servicecontroller");
+const { route } = require("./serviceController");
 
 
 
