adcce1d85f74acb19927bd6b7db3629ed625d99e
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-09-27 17:06:45 -0400

    aaaa

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const { route } = require("./serviceController");
 
 
 
